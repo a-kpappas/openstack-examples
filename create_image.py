33e9e3c39a365c36a2228437a908f740df56314b
Anton Pappas <apappas@suse.com>
2019-03-29 17:49:04 +0100

    Can now create and delete images.
    TODO: Prompts?
    TODO: Turn everything into proper functions.

--- create_image.py
+++ create_image.py
@@ -11,12 +11,20 @@ openstack.enable_logging(debug=True)
 # ~/.config/openstack/clouds.yaml or /etc/openstack/clouds.yaml.
 cloud = openstack.connect(cloud='engineering')
 
-# Create new instance
-# The image and flavor is specific to the OpenStack service.
-# See the flavors_and_images.py example on how to list the available 
-# images and flavors
-image_name='apappas_refhost_image'
+
+image_name ='apappas_refhost_image'
+file_name = '/home/akpappas/openstack/SLES12-SP4-JeOS.x86_64-12.4-OpenStack-Cloud-GM-clone.qcow2'
 cloud_image = cloud.get_image(image_name);
+image_deleted = False;
 if cloud_image is None:
-    cloud_image = cloud.create_image('apappas_refhost_image', filename ='SLES12-SP4-JeOS.x86_64-12.4-OpenStack-Cloud-GM.qcow2',disk_format='qcow')
-                                     
+    cloud_image = cloud.create_image(image_name, filename = file_name,disk_format='qcow2')
+else:
+    # This is smelly and will be removed!
+    image_deleted = cloud.delete_image(image_name, wait=True)
+if image_deleted == True:
+    print('Image deleted.')
+else:
+    print ('Image uploaded')
+    
+     
+                           
