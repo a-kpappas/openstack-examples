ac0857e9ae2b7b6414a7ee96c31628119cb911a5
Anton Pappas <apappas@suse.com>
2019-04-23 19:21:10 +0200

    Added script to dl the JeOS image, add root user and team keys to it.
    TODO: libguestfs cleanup.

---
+++ add_users.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 15:48:58 2019
+
+@author: akpappas
+"""
+import os
+import guestfs
+import wget
+import subprocess
+
+from shutil import copy
+
+image_name = 'SLES12-SP4-JeOS.x86_64-12.4-OpenStack-Cloud-GM.qcow2';
+if not os.path.isfile(image_name):
+    url = 'http://download.suse.de/install/SLE-12-SP4-JeOS-GM/'+image_name;
+    image_name = wget.download(url);
+original_image = image_name;
+image_name = image_name + 'edited';
+copy(original_image, image_name);
+
+if os.path.isfile('authorized_keys'):
+    os.remove('authorized_keys');
+
+wget.download('http://qam.suse.de/downloads/authorized_keys');
+    
+g = guestfs.GuestFS(python_return_dict=True);
+g.add_drive(image_name);
+g.launch();
+g.mount('/dev/sda3','/');
+g.download('/etc/cloud/cloud.cfg','/tmp/cloud.cfg');
+subprocess.call("sed 's/^disable_root:[^:]\+/disable_root: false/' /tmp/cloud.cfg > /tmp/cloud.cfg.new",shell=True);
+g.upload('/tmp/cloud.cfg.new', '/tmp/cloud.cfg.new');
+g.mkdir('/root/.ssh');
+g.chmod(0o700,'/root/.ssh');
+g.upload('authorized_keys','/root/.ssh/authorized_keys');
+g.chmod(0o600,'/root/.ssh/authorized_keys');
+
+    
